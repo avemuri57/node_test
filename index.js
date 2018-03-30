42aecffca74f61228ec7bc7d68b8c7e048307fd0
Anand Vemuri <avemuri57@gmail.com>
2018-03-30 14:23:25 -0700

    add first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+var app = express();
+
+var PORT = process.env.PORT || 5050;
+
+
+app.get('/',function(req,res){
+	res.send({"Hello":"World"});
+});
+
+app.listen(PORT,function(){
+	console.log("Listening on port "+PORT);
+});
